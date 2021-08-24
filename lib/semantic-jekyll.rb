6705f34ce04750caab0c38db2cb6d50c4814df27
manunamz <manunamz@pm.me>
2021-08-24 18:44:28 -0400

    🚀 init commit.

---
+++ lib/semantic-jekyll.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "semantic-jekyll/version"
+
+module Jekyll
+  module Semantic
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/semantic-jekyll/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Jekyll
+  module Semantic
+
+    VERSION = "0.0.1"
+
+  end
+end
